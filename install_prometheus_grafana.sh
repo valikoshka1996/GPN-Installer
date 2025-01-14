2aeb971f205f735b97221ef9f675caa810761aeb
Valentyn Shkarupa <115169564+valikoshka1996@users.noreply.github.com>
2025-01-14 20:04:50 +0200

    Update install_prometheus_grafana.sh
    HotFix

--- install_prometheus_grafana.sh
+++ install_prometheus_grafana.sh
@@ -71,7 +71,8 @@ EOF'
 
 # Додавання репозиторію для Grafana
 echo "Додавання репозиторію Grafana..."
-sudo apt-get install -y software-properties-common
+sudo apt-get install -y gnupg2
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
 sudo apt-get update
 
